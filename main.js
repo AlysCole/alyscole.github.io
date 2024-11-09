93c7774d7e65821586c99238faf155f301758dcc
AlysCole <alyssanqa@gmail.com>
2024-11-09 15:33:40 +0800

    Add initial functions to check whether link is scrolled into

--- main.js
+++ main.js
@@ -23,11 +23,37 @@ window.addEventListener("load", () => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-        
+
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
         });
     });
 
+    function isScrolledIntoView(el) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+
+        // Only completely visible elements return true:
+        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        // Partially visible elements return true:
+        //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+        return isVisible;
+    }
+
+    const sidebarLinks = document.querySelectorAll(".sidebar__nav > ul > li > a");
+    const sidebarLinkEls = {
+        aboutMe: document.querySelector(".sidebar__nav > ul > li > .nav__link.about"),
+        projects: document.querySelector(".sidebar__nav > ul > li > .nav__link.projects"),
+        contact: document.querySelector(".sidebar__nav > ul > li > .nav__link.contact")
+    }
+
+    document.addEventListener("scroll", () => {
+        // Check which section is scrolled into view
+        const aboutMeSection = document.getElementById("#about-me");
+        const projectsSection = document.getElementById("#projects");
+        // const contactMeSection = document.getElementById("#contact-me");
+
+    })
 });
