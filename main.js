b833e52baf2bc999a8c09bd1e1929dd7800a7dd4
AlysCole <alyssanqa@gmail.com>
2024-11-09 15:16:41 +0800

    Fixed sidebar scrolling, projects styling

--- main.js
+++ main.js
@@ -19,4 +19,15 @@ window.addEventListener("load", () => {
     
       squareContainer.appendChild(squareDiv);
     }
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+        
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
 });
