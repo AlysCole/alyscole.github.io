16a3fe47824f30ba7c48b6f7adae004a8f9f0fa5
AlysCole <alyssanqa@gmail.com>
2024-11-09 14:25:58 +0800

    Add hero and about me section

---
+++ main.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+    const dimensions = document.body.getBoundingClientRect();
+    const screenWidth = dimensions.height;
+    const screenHeight = dimensions.width;
+
+    console.log("Window dimensions:", screenWidth, screenHeight);
+
+    const squareColumns = Math.floor(screenWidth / (62));
+    const squareRows = Math.round(screenHeight / (62));
+    const numberOfSquares = squareColumns * squareRows;
+
+    console.log("Square Columns / Square Rows:", squareColumns, "/", squareRows);
+
+    const squareContainer = document.querySelector(".square-container");
+
+    for (let i = 0; i < numberOfSquares; i++) {
+      const squareDiv = document.createElement("div");
+      squareDiv.classList.add("square");
+    
+      squareContainer.appendChild(squareDiv);
+    }
+});
